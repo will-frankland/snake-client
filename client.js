6d82fa2b3ab33d123375588f75f37a8aa7123064
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-03-16 13:33:18 -0400

    import/export changes

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541',
+  });
+
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,25 @@
 const net = require("net");
 
+
+connect = require("./client");
+
+
+
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: '50541',
+//   });
 
 
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(data);
-  })
+//   conn.setEncoding("utf8");
+//   conn.on("data", (data) => {
+//     console.log(data);
+//   })
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
